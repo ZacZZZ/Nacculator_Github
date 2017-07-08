821eb716706036878bcc3694b5c60c1f798bd01f
ZacZZZ <zaczhang@umich.edu>
2017-07-08 18:31:39 -0400

    Flask download module added!

--- nacc/nacculator_flask.py
+++ nacc/nacculator_flask.py
@@ -5,32 +5,24 @@ import argparse
 import os
 
 
-from flask import Flask, render_template, request, redirect, url_for, Response
+from flask import Flask, render_template, request, redirect, url_for, \
+send_from_directory #, Response
 
 # To operate the uploaded file
 from werkzeug.utils import secure_filename
 from werkzeug import secure_filename
 
-# UPLOAD_FOLDER = '/path/to/the/uploads'
-# ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+# from http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
-
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-
-
-app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 email_addresses = []
 redcapWarnings = []
 
-@app.route('/large.csv')
-def generate_large_csv():
-    def generate():
-        for row in iter_all_rows():
-            yield ','.join(row) + '\n'
-    return Response(generate(), mimetype='text/csv')
-
+# Main Page
 @app.route('/')
 def hello_world():
     author = "Zac"
@@ -41,23 +33,42 @@ def hello_world():
 # def upload_file():
    # return render_template('upload.html')
 
+##### The upload and download part is somewhat tricky.
+# Resources:
+# http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+# https://stackoverflow.com/questions/24577349/flask-download-a-file
+# Additional resource that might be helpful
+# https://stackoverflow.com/questions/27628053/uploading-and-downloading-files-with-flask
+
+# The upload action/button
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      # print(f)
-      # print(secure_filename(f.filename))
-      # cwd = os.getcwd() # working directory is where the python code is
-      # print(cwd)
-      # redcap2nacc.main(f.filename)
-      # redcapWarnings = redcap2nacc_flask.main(f.filename)
-      # print (redcap2nacc_flask.main(f.filename))
-      redcapWarnings = redcap2nacc_flask.main(f.filename)
-      # 'file uploaded successfully, Nacc form converted. Please check /warnings.html for conversion warnings.'+ "\n" + 'Find the converted file: ' + 'NaccConverted_' + f.filename[:-4] + '.txt'
+      file = request.files['file']
+      if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+      if file:
+          somefilename = secure_filename(file.filename)
+        #   file.save(secure_filename(file.filename))
+          file.save(os.path.join(app.config['UPLOAD_FOLDER'], somefilename))
+      redcapWarnings = redcap2nacc_flask.main(file.filename)
+      # 'file uploaded successfully, Nacc form converted. Please check /warnings.html for conversion warnings.'+ "\n" + 'Find the converted file: ' + 'NaccConverted_' + file.filename[:-4] + '.txt'
     #   return redirect('/warnings.html')
-      return render_template('warnings.html', warnings=redcapWarnings, filename=f.filename[:-4])
+      return render_template('warnings.html', warnings=redcapWarnings, \
+      filename=file.filename[:-4], downloadname='NaccConverted_' + \
+      file.filename[:-4] + '.txt')
 
+@app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    # uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
+    uploads = app.root_path
+    return send_from_directory(directory=uploads, filename=filename)
+
+# @app.route('/uploads/<filename>')
+# def uploaded_file(filename):
+#     return send_from_directory(app.config['UPLOAD_FOLDER'],
+#                                filename)
 
 @app.route('/signup', methods = ['POST'])
 def signup():
@@ -70,16 +81,26 @@ def signup():
 @app.route('/warnings.html')
 def warning():
 
-    return render_template('warnings.html', warnings=redcapWarnings, filename=f.filename[:-4])
+    return render_template('warnings.html', warnings=redcapWarnings, \
+    filename=file.filename[:-4])
 
 
 @app.route('/emails.html')
 def emails():
     return render_template('emails.html', email_addresses=email_addresses)
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
+
 if __name__ == '__main__':
     #Call nacculator and process data uploaded from user
-
-		# raw_csv = argparse.ArgumentParser(description='Process redcap form output to nacculator.')
-
+    # raw_csv = argparse.ArgumentParser(description='Process redcap form output to nacculator.')
     app.run()
